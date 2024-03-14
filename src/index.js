2b5b35a6ae2613183b22a9c581fde2f42174ad6f
Reya <150582472+Reya0707@users.noreply.github.com>
2024-03-14 22:07:01 +0000

    Added some additional prompt instructions

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions"); // Corrected selector
   let apiKey = "36c26f017e4ae34tb6a9679bb25odafe";
   let context =
-    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 4 lines only. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
+    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 7 lines only. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
