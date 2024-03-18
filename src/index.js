ca292bec93c32548d0b2fbb0399a15344c1893bb
Reya <150582472+Reya0707@users.noreply.github.com>
2024-03-18 15:57:55 +0000

    added some basic styling to my project.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-function displayPoem(response) {
+function displayWaitingMessage() {
+  document.getElementById("poem-response").innerHTML = "Generating poem...";
+}
 
-  new Typewriter("#poem", {
+function displayPoem(response) {
+  let poemResponse = document.getElementById("poem-response");
+  poemResponse.innerHTML = ""; 
+  new Typewriter(poemResponse, {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 0.5,
-    cursor:"",
+    cursor: ""
   });
 }
+
 
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#user-instructions"); // Corrected selector
+  let instructionsInput = document.querySelector("#user-instructions"); 
   let apiKey = "36c26f017e4ae34tb6a9679bb25odafe";
   let context =
-    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 7 lines only. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
+    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 5 lines only. Sign the peom at the end with Reya AI and make sure it's bold. Provide a title for each short poem and font size should be 15px. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
