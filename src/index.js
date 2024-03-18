bf225bd3a5eaa92ec92710c258d23c963cbd0893
Reya <150582472+Reya0707@users.noreply.github.com>
2024-03-18 16:02:06 +0000

    Updated the AI prompt

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 0.5,
+    delay: 1,
     cursor: ""
   });
 }
@@ -21,7 +21,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions"); 
   let apiKey = "36c26f017e4ae34tb6a9679bb25odafe";
   let context =
-    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 5 lines only. Sign the peom at the end with Reya AI and make sure it's bold. Provide a title for each short poem and font size should be 15px. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
+    "You are an AI Poet that loves to tell short poems. The poem must be provided in HTML format with 5 lines only. Sign the poem at the end with Reya AI and make sure only Reya AI is bold. Provide a title for each short poem and font size should be 15px and bold. Make sure the follow the user instructions. Example: <p>this is a poem</p>.";
   let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
   
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
